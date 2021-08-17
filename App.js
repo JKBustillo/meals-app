ebb66cc6e668f6ab8b75973c5a678d429eaf3818
Jose Bustillo <jabustillo@uninorte.edu.co>
2021-08-17 15:04:26 -0500

    feat: load fonts

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  void (async () => {
+    await Font.loadAsync({
+      'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+      'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+    });
+    setFontsLoaded(true);
+  })();
+
+  if (!fontsLoaded) {
+    return <AppLoading onError={(err) => console.log(err)} />;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
