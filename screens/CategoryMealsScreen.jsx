6818fd0d1c0230b68b07b3e6bedb54a2999d6cce
Jose Bustillo <jabustillo@uninorte.edu.co>
2021-08-18 15:20:11 -0500

    feat: add navigation to mealDetails and set headerTitle

--- screens/CategoryMealsScreen.jsx
+++ screens/CategoryMealsScreen.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
 
 const CategoryMealsScreen = props => {
+  const categoryId = props.navigation.getParam('categoryId');
+  const selectedCategory = CATEGORIES.find(cat => cat.id === categoryId);
+
   return (
     <View style={styles.screen}>
+      <Text>{selectedCategory.title}</Text>
       <Text>Category Meals Screen</Text>
+      <Button
+        title="Go to Meals!"
+        onPress={() => {
+          props.navigation.navigate('MealDetails');
+        }}
+      />
     </View>
   );
+};
+
+CategoryMealsScreen.navigationOptions = navigationData => {
+  const categoryId = navigationData.navigation.getParam('categoryId');
+
+  const selectedCategory = CATEGORIES.find(cat => cat.id === categoryId);
+
+  return {
+    headerTitle: selectedCategory.title
+  };
 };
 
 const styles = StyleSheet.create({
